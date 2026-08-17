b0a056af2c9ca13fefbb8ae40cbfa99785351085
talianappin <talia.napierala@gmail.com>
2026-08-17 22:02:00 +0200

    flake update rollback

--- modules/apps/yazi.nix
+++ modules/apps/yazi.nix
@@ -1,20 +1,12 @@
 { inputs, ... }:
 {
-  apps.yazi.homeManager.programs.yazi =
+  apps.yazi.homeManager = 
   { pkgs, ... }:
   {
-    enable = true;
-    enableFishIntegration = true;
-    extraPackages = with pkgs;
-    [
-      glow
-      ouch
-      ffmpeg
-      jq
-      fd
-      rg
-      fzf
-      resvg
-    ];
+    programs.yazi =
+    {
+      enable = true;
+      enableFishIntegration = true;
+    };
   };
-};
+}
--- modules/sys/wayland/niri.nix
+++ modules/sys/wayland/niri.nix
@@ -15,7 +15,7 @@
 	  keyboard = {
 	    xkb = {
 	      layout = "pl";
-	      variant = "qwerty";
+	      variant = "prog";
 	    };
             track-layout = "global";
             repeat-delay = 600;
@@ -25,7 +25,7 @@
 	    natural-scroll = false;
 	    accel-speed = 0;
 	  };
-	  workspace-auto-back-and-forth = true;
+#	  workspace-auto-back-and-forth = true;
           focus-follows-mouse.enable = true;
 	};
 	outputs = {
@@ -50,12 +50,14 @@
             width = 2;
 	    active.color = "#${config.colorScheme.palette.base02}";
 	    urgent.color = "#${config.colorScheme.palette.base01}";
-	  };  
+	  };
+	  default-column-width = { proportion = 0.5; };
 	};
 	binds = {
           "Mod+T".action.spawn = "foot";
           "Mod+D".action.spawn = "fuzzel";
           "Mod+Q".action.close-window = [];
+	  "Mod+E".action.spawn = ["foot" "yazi"];
       
           "Mod+Left".action.focus-column-left = [];
           "Mod+Right".action.focus-column-right = [];
@@ -70,6 +72,17 @@
 	  "Mod+F".action.maximize-column = [];
 #	   "Mod+Ctrl+F".action.maximize-window-to-edges = [];  v25.11 feature (on v25.08)
 	  "Mod+Shift+F".action.fullscreen-window = [];
+
+	  "XF86AudioRaiseVolume" = {
+            action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"];
+            allow-when-locked = true;
+            cooldown-ms = 150;
+          };
+          "XF86AudioLowerVolume" = {
+            action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
+            allow-when-locked = true;
+            cooldown-ms = 150;
+          };
         };
       };
       services = {
